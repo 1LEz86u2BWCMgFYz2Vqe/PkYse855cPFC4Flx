278a64771048616d132e299ef25c23e8ce4d95ee
me <felix.lacroix@umontreal.ca>
2025-07-20 15:25:18 -0400

    fix

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log("hi");
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const SteamTotp = require('steam-totp');
+const SteamUser = require('steam-user');
+const steamClient = new SteamUser();
+
+const LogInSteam = () => {
+    console.log("logging in steam");
+}
+LogInSteam();
+
+app.use(express.static("public"));
+app.use(express.json());
+
+var queue = [];
+app.get("/", async function(req, res) {
+	res.send(queue[0]);
+	queue.shift();
+});
+app.listen(process.env.PORT);
